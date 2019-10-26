36d3bcf50cb6e9ab520fa6cd50d895be733b7269
Tri Aribowo <t.rahmat.aribowo@gmail.com>
2019-10-26 11:03:53 +0700

    first app-01

---
+++ README.md
@@ -0,0 +1,6 @@
+# ReactJS
+
+- install babel-cli
+- install babel-preset-react
+- install babel-preset-env
+- bable ..source --out-file=destination --presets=env,react --watch
---
+++ src/app.js
@@ -0,0 +1,12 @@
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>this is some info</p>
+        <ol>
+            <li>List one</li>
+            <li>List two </li>
+        </ol>
+    </div>
+);
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
